e5e9f296bba100fa9b579c13f907599682fc0f92
Financialstream <financialstreamllc@gmail.com>
2026-02-25 17:34:16 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -531,3 +531,7 @@ window.location.href = buildLangUrl(lang);
     window.history.replaceState({}, document.title, clean);
   }
 })();
+
+
+// Chatbase widget loader (Financial Stream Assistant)
+(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="L9Rqcw-6NJyxiL2AcTbtP";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
